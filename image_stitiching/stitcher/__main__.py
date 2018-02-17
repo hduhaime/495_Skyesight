06dbcd5f5b53bf32f54fc1be6f27359c3822575e
henry <hduhaime@umich.edu>
2018-02-17 15:15:14 -0500

    Minor driver change, yosemite test images

--- image_stitiching/stitcher/__main__.py
+++ image_stitiching/stitcher/__main__.py
@@ -5,14 +5,16 @@ import os
 import argparse
 import imutils
 import cv2
+import code
+import sys
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('-i', '--input-dir', required=True, help='Path to image input directory')
     args = vars(ap.parse_args())
 
-    input_dir = args['input_dir']
-
+    dir = args['input_dir'].strip(' ')
+    input_dir = os.path.join(os.getcwd(), dir)
     image_files = sorted(os.listdir(input_dir))
     image_files = [os.path.join(input_dir, x) for x in image_files]
     print(image_files)
