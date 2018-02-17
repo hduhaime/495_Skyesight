5d4c8bcbdc88a8852740a4475761318b442ee99f
Eric Bogard <ebogard@umich.edu>
2018-02-16 19:26:29 -0500

    stuff

--- image_stitiching/stitcher/__main__.py
+++ image_stitiching/stitcher/__main__.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import imutils
 import cv2
+from stitcher.impl.__main__ import Stitcher
 
 def main():
     ap = argparse.ArgumentParser()
@@ -15,7 +16,13 @@ def main():
 
     image_files = sorted(os.listdir(input_dir))
     image_files = [os.path.join(input_dir, x) for x in image_files]
-    print(image_files)
+    images = [imutils.resize(cv2.imread(x), width=400) for x in image_files]
+
+    stitcher = Stitcher()
+    result = stitcher.stitch(images)
+
+    cv2.imshow("Result", result)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
---
+++ image_stitiching/stitcher/impl/__main__.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+
+class Stitcher:
+
+    def stitch(self, images):
+        (imageL, imageM, imageR) = images
+        ratio = 0.75
+        reprojThresh = 4.0
+        
+        canvas_dim = max(imageM.shape[0], imageM.shape[1]) * len(images)
+
+        (kpL, ftL) = self.describe(imageL)
+        (kpM, ftM) = self.describe(imageM)
+        (kpR, ftR) = self.describe(imageR)
+
+        M1 = self.match(kpL, kpM, ftL, ftM, ratio, reprojThresh) 
+        M2 = self.match(kpR, kpM, ftR, ftM, ratio, reprojThresh) 
+
+
+        (matches, H1, status) = M1
+        (matches, H2, status) = M2
+
+        
+
+        resultA = cv2.warpPerspective(imageL, H1, (canvas_dim, canvas_dim))
+        resultB = cv2.warpPerspective(imageR, H2, (canvas_dim, canvas_dim))
+
+        
+
+        resultB[0:imageM.shape[0], 0:imageM.shape[1]] = imageM
+
+
+        return resultB
+
+    def describe(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        descriptor = cv2.xfeatures2d.SIFT_create()
+        (keypoints, features) = descriptor.detectAndCompute(image, None)
+        keypoints = np.float32([kp.pt for kp in keypoints])
+        return (keypoints, features)
+
+    def match(self, kp1, kp2, ft1, ft2, ratio, reprojThresh):
+        matcher = cv2.DescriptorMatcher_create("BruteForce")
+        rawMatches = matcher.knnMatch(ft1, ft2, 2)
+        matches = []
+        for m in rawMatches:
+            if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+                matches.append((m[0].trainIdx, m[0].queryIdx))
+
+        if len(matches) > 4:
+            pts1 = np.float32([kp1[i] for (_, i) in matches])
+            pts2 = np.float32([kp2[i] for (i, _) in matches])
+            (H, status) = cv2.findHomography(pts1, pts2, cv2.RANSAC, reprojThresh)
+            return (matches, H, status)
+
+        return None
+
+    def drawMatches(self, imageA, imageB, kpsA, kpsB, matches, status):
+        # initialize the output visualization image
+        (hA, wA) = imageA.shape[:2]
+        (hB, wB) = imageB.shape[:2]
+        vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
+        vis[0:hA, 0:wA] = imageA
+        vis[0:hB, wA:] = imageB
+    
+        # loop over the matches
+        for ((trainIdx, queryIdx), s) in zip(matches, status):
+                # only process the match if the keypoint was successfully
+                # matched
+                if s == 1:
+                        # draw the match
+                        ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
+                        ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
+                        cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
+    
+        # return the visualization
+        return vis 
+
