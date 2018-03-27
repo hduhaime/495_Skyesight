b735f5545865ca7233fbe8cab8ba847bfaf6344a
Eric Bogard <ebogard@umich.edu>
2018-03-27 10:45:44 -0400

    better files

---
+++ image_stitching/setup.py
@@ -0,0 +1,18 @@
+"""
+Skysight image stitching
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='skysight-stitch',
+    version='0.1.0',
+    packages=['stitcher'],
+    include_package_data=True,
+    install_requires=[
+        'opencv-contrib-python',
+        'imutils',
+        'argparse',
+    ]
+)
