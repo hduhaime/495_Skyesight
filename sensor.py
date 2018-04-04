3606d153ac7713648eec3af6f2a680c3216dd63e
Gaurav Vyas <geevee@umich.edu>
2018-04-04 15:06:10 -0400

    Sensor is now a wrapper class

--- sensor.py
+++ sensor.py
@@ -1,41 +1,74 @@
 import RPi.GPIO as GPIO
 import time
+from threading import Thread, Lock
 
-def getReading():
-    GPIO.setmode(GPIO.BCM)
-    TRIG= 4
-    ECHO = 18
+class Sensor:
 
-    print('Distance Measurement in Progress')
+    def __init__(self):
+        self.distance = 0
+        self.mutex = Lock()
+        self.threshold = 1
 
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.setup(ECHO, GPIO.IN)
+    def setThreshold(self, threshold):
 
-    GPIO.output(TRIG, False)
-    print('waiting for sensor')
-    time.sleep(2)
+        self.mutex.acquire()
 
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
+        try:
+            self.threshold = threshold
+        finally:
+            self.mutex.release()
 
-    while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
+    def getReading(self):
+        self.mutex.acquire()
+        
+        try:
+            if self.distance < self.threshold:
+                return self.distance
+            else:
+                return None
+        finally:
+            self.mutex.release()
 
-    while(GPIO.input(ECHO) == 1):
-        pulse_end = time.time()
 
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration*17150
-    distance = round(distance,2)
+    def startSensors(self):
 
-    print('Distance:',distance,'cm')
+        GPIO.setmode(GPIO.BCM)
+        TRIG= 4
+        ECHO = 18
+        
+        # while True:
 
-    GPIO.cleanup()
-    
-    if distance < 100:
-        return distance/100
-    else:
-        return None
+        #     GPIO.setup(TRIG, GPIO.OUT)
+        #     GPIO.setup(ECHO, GPIO.IN)
+
+        #     GPIO.output(TRIG, False)
+        #     print('waiting for sensor')
+        #     time.sleep(2)
+
+        #     GPIO.output(TRIG, True)
+        #     time.sleep(0.00001)
+        #     GPIO.output(TRIG, False)
+
+        #     while GPIO.input(ECHO) == 0:
+        #         pulse_start = time.time()
+
+        #     while(GPIO.input(ECHO) == 1):
+        #         pulse_end = time.time()
+
+        #     pulse_duration = pulse_end - pulse_start
+        #     distance = pulse_duration*17150
+        #     distance = round(distance,2)
+
+        #     print('Distance:',distance,'cm')
+
+        #     GPIO.cleanup()
+        
+        #     dist_in_metres = distance/100
+
+        #     self.mutex.acquire()
+        #     if dist_in_metres < self.threshold:
+        #         self.distance = dist_in_metres
+            
+        #     self.mutex.release()
     
         
