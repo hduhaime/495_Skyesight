ffdbfe00d147f1fb5fe48da53a4c0ccd6d37952c
Sahil Desai <sahild@umich.edu>
2018-04-04 14:05:05 -0400

    refactored project and fixed threading problems

---
+++ Util.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class DisplaySelection(Enum):
+    MainLeft = 0
+    Right = 1
+
+
+class OnScreenButtons(Enum):
+    FullSplitscreenToggle = 0
+    Recalibrate = 1
+    ToggleNotifications = 2
+    MainLeftPrevFeed = 3
+    MainLeftNextFeed = 4
+    RightPrevFeed = 5
+    RightNextFeed = 6
+
